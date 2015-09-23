e8246b4db1468f1fd7b960d5c1142ee99d343c20
Thomas Hourlier <thomas.hourlier@cnode.fr>
2015-09-23 12:02:36 +0200

    Added support to es6 through babel and js linters.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
 var gulp = require('gulp'),
 	bs = require('browser-sync').create(),
-	bg = require("gulp-bg"),
-	ps = require('ps-node');
+	bg = require('gulp-bg'),
+	ps = require('ps-node'),
+  babel = require('gulp-babel'),
+  sourcemaps = require('gulp-sourcemaps'),
+  crisper = require('gulp-crisper'),
+  gulpif = require('gulp-if'),
+  jshint = require('gulp-jshint');
 
 var POLYSERVE_PORT = 8080,
     elementName = 'seed-element';
@@ -12,18 +17,41 @@ var browserSyncConfig = function(path, cb) {
     startPath: path
   });
 
-  bs.reload;
+  bs.reload();
 
   process.on('exit', function() {
     bs.exit();
   });
+  cb();
 };
 
 var watchComponent = function() {
-  gulp.watch(['./*.html', 'demo/**/*.html', 'test/**/*.html'], bs.reload);
+  gulp.watch(['./src/*.{js,html}', 'demo/**/*.html', 'test/**/*.html'], ['js', 'jshint', bs.reload]);
 };
 
-gulp.task('polyserve', function(cb) {
+// Transpile all JS to ES5.
+gulp.task('js', function () {
+  return gulp.src(['src/**/*.{js,html}'])
+    .pipe(sourcemaps.init())
+    .pipe(gulpif('*.html', crisper())) // Extract JS from .html files
+    .pipe(gulpif('*.js', babel()))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('.'));
+});
+
+// Lint JavaScript
+gulp.task('jshint', function () {
+  return gulp.src([
+      'src/**/*.{js,html}',
+      'gulpfile.js'
+    ])
+    .pipe(jshint.extract()) // Extract JS from .html files
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(gulpif(!bs.active, jshint.reporter('fail')));
+});
+
+gulp.task('polyserve', ['js', 'jshint'], function(cb) {
   ps.lookup({
     command: 'polyserve',
     psargs: '-f'
